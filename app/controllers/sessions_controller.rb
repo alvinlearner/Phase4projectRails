eb6481ea3f83adacffae57c6ee30a2c06e3bc953
alvinkenyagah <alvin47k@outlook.com>
2023-07-11 21:32:35 +0300

    Update sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,44 +1,23 @@
-# class SessionsController < ApplicationController
-#     # Create a session to hold user info when loging in and authenticate user
-#     def create
-#         user = User.find_by(username: params[:username])
-#         if user&.authenticate(params[:password])
-#           session[:user_id] = user.id
-#           session[:username] = user.username
-#           session[:email] = user.email
-#           render json: user, status: :created
-#         else
-#           render json: { error: "Invalid username or password" }, status: :unauthorized
-#         end
-#     end
-
-#     # Remove user details from sessions when logging out
-
-
-#     def destroy
-#       session.delete :user_id
-#       head :no_content
-#     end
-# end
-
 class SessionsController < ApplicationController
-    def create
-      user = User.find_by(username: params[:username])
-      if user&.authenticate(params[:password])
-        session[:user_id] = user.id
-        render json: user, status: :created
-      else
-        render json: { errors: ["Invalid username or password"] }, status: :unauthorized
-      end
+  def create
+    user = User.find_by(username: params[:username])
+    if user&.authenticate(params[:password])
+      session[:user_id] = user.id
+      # Include the user's authentication token in the JSON response if applicable
+      auth_token = user.authentication_token if user.respond_to?(:authentication_token)
+      render json: { user: user, auth_token: auth_token }, status: :created
+    else
+      render json: { errors: ["Invalid username or password"] }, status: :unauthorized
+    end
+  end
+
+  def destroy
+    unless session[:user_id]
+      render json: { errors: ["Not logged in"] }, status: :unauthorized
+      return
     end
 
-    def destroy
-        unless session[:user_id]
-          render json: { errors: ["Not logged in"] }, status: :unauthorized
-          return
-        end
-    
-        session.delete(:user_id)
-        head :no_content
-    end
+    session.delete(:user_id)
+    head :no_content
+  end
 end
