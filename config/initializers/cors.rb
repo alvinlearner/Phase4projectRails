1d278bbe05ce889c8d3759a4297e7ddf1a30ebca
alvinkenyagah <alvin47k@outlook.com>
2023-07-13 13:42:59 +0300

    Configure CORS

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-      origins 'https://main--velvety-sawine-df90ba.netlify.app/' # Update this with the domains allowed to access your API
+      origins '*' # Update this with the domains allowed to access your API
       resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head]
     end
   end
